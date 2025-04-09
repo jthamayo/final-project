a6f7ae3ed0b91dac5311e84c214171d1e9a984e8
jthamayo <jtamayobalogh@gmail.com>
2025-04-09 21:22:13 +0200

    feat: add UserController and implement createUser endpoint using UserService

---
+++ backend/src/main/java/io/github/jthamayo/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package io.github.jthamayo.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.jthamayo.backend.dto.UserDto;
+import io.github.jthamayo.backend.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+	this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+	UserDto savedUser = userService.createUser(userDto);
+	return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+
+    }
+}
