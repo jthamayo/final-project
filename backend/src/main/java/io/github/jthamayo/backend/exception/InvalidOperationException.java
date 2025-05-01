3e6a9b516f19733bf18fdf55a80b3960ef9e8f5e
jthamayo <jtamayobalogh@gmail.com>
2025-05-01 21:21:04 +0200

    refactor: create IvalidOperationException to handle operations that violate business logic

---
+++ backend/src/main/java/io/github/jthamayo/backend/exception/InvalidOperationException.java
@@ -0,0 +1,13 @@
+package io.github.jthamayo.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class InvalidOperationException extends RuntimeException {
+
+    public InvalidOperationException(String message) {
+	super(message);
+    }
+
+}
