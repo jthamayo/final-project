158ec63fde54e766cf1928313c189d85a1a60712
jthamayo <jtamayobalogh@gmail.com>
2025-05-08 00:00:03 +0200

    fix: add missing properties and handle null instances

--- backend/src/main/java/io/github/jthamayo/backend/mapper/UserMapper.java
+++ backend/src/main/java/io/github/jthamayo/backend/mapper/UserMapper.java
@@ -1,5 +1,8 @@
 package io.github.jthamayo.backend.mapper;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 import io.github.jthamayo.backend.dto.UserDto;
 import io.github.jthamayo.backend.entity.User;
 
@@ -7,7 +10,10 @@ public class UserMapper {
 
     public static UserDto mapToUserDto(User user) {
 	return new UserDto(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getNumber(),
-		user.getGroup() != null ? user.getGroup().getId() : null);
+		user.getGroup() != null ? user.getGroup().getId() : null,
+		user.getJobs() != null ? user.getJobs().stream().map((job) -> job.getId()).collect(Collectors.toList())
+			: new ArrayList<>(),
+		user.getHomeAddress() != null ? user.getHomeAddress().getId() : null);
     }
 
     public static User mapToUser(UserDto userDto) {
