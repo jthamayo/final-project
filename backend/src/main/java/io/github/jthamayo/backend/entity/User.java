e5eb2d08c84dda5d179b230ca0bf9ffb9569693e
jthamayo <jtamayobalogh@gmail.com>
2025-05-11 01:40:43 +0200

    feat: extend User entity with new fields and role relationship

--- backend/src/main/java/io/github/jthamayo/backend/entity/User.java
+++ backend/src/main/java/io/github/jthamayo/backend/entity/User.java
@@ -1,6 +1,8 @@
 package io.github.jthamayo.backend.entity;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -12,6 +14,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -25,23 +29,39 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
     @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable = false, unique = true)
-    private String number;
+
+    private String password;
+
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
     @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Job> jobs;
-    @OneToOne(optional = true, cascade = CascadeType.ALL)
+
+    @OneToOne(optional = true, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "home_address_id")
     private Address homeAddress;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     ////////////////// GETTERS&SETTERS///////////////////////
 
@@ -49,8 +69,8 @@ public class User {
 	return this.id;
     }
 
-    public String getNumber() {
-	return number;
+    public String getphoneNumber() {
+	return phoneNumber;
     }
 
     public Group getGroup() {
@@ -89,8 +109,8 @@ public class User {
 	this.lastName = lastName;
     }
 
-    public void setNumber(String number) {
-	this.number = number;
+    public void setphoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
     }
 
     public void setEmail(String email) {
@@ -109,35 +129,70 @@ public class User {
 	this.homeAddress = homeAddress;
     }
 
+    public String getUsername() {
+	return username;
+    }
+
+    public void setUsername(String userName) {
+	this.username = userName;
+    }
+
+    public String getPassword() {
+	return password;
+    }
+
+    public void setPassword(String password) {
+	this.password = password;
+    }
+
+    public String getPhoneNumber() {
+	return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Role> getRoles() {
+	return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+	this.roles = roles;
+    }
+
     //////////////// CONSTRUCTOR////////////////
 
     public User() {
-	
+
     }
 
-    public User(String firstName, String lastName, String email, String number) {
+    public User(String firstName, String lastName, String email, String phoneNumber) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.email = email;
-	this.number = number;
+	this.phoneNumber = phoneNumber;
 
     }
 
-    public User(Long id, String firstName, String lastName, String email, String number) {
+    public User(Long id, String firstName, String lastName, String username, String email, String phoneNumber) {
 	this.id = id;
 	this.firstName = firstName;
 	this.lastName = lastName;
+	this.username = username;
 	this.email = email;
-	this.number = number;
+	this.phoneNumber = phoneNumber;
 
     }
 
-    public User(Long id, String firstName, String lastName, String email, String number, Group group, List<Job> jobs,
-	    Address homeAddress) {
+    public User(Long id, String firstName, String lastName, String username, String email, String phoneNumber,
+	    Group group, List<Job> jobs, Address homeAddress) {
 	this.id = id;
 	this.firstName = firstName;
 	this.lastName = lastName;
+	this.username = username;
 	this.email = email;
+	this.phoneNumber = phoneNumber;
 	this.group = group;
 	this.jobs = jobs;
 	this.homeAddress = homeAddress;
