3a2240cac6e326c405ca17b5f4ad42248753ca52
jthamayo <jtamayobalogh@gmail.com>
2025-05-16 08:34:14 +0200

    feat: add useForm to handle login credentials submission

--- frontend/src/components/auth/LoginComponent.tsx
+++ frontend/src/components/auth/LoginComponent.tsx
@@ -1,4 +1,48 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type LoginValues = {
+  usernameOrEmail: string;
+  password: string;
+};
+
 const LoginComponent = () => {
-  return <div className="text-blue-600 text-5xl my-5">Login</div>;
+  const { register, handleSubmit } = useForm<LoginValues>();
+  const onSubmit: SubmitHandler<LoginValues> = (data) => console.log(data);
+  return (
+    <>
+      <h2 className="text-white font-title text-4xl text-center"> Login</h2>
+      <div className=" my-5 w-120 h-100 bg-white rounded-4xl">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col justify-between h-full p-15"
+        >
+          <label htmlFor="usernameOrEmail" className="font-title text-xl">
+            Username or Email
+          </label>
+          <input
+            type="text"
+            {...register("usernameOrEmail")}
+            placeholder="Username or Email"
+            className="bg-[#EDECF6] text-black w-full h-1/5 pl-5 rounded-xl"
+          />
+          <label htmlFor="password" className="font-title text-xl">
+            Password
+          </label>
+          <input
+            type="password"
+            {...register("password")}
+            placeholder="Password"
+            className="bg-[#EDECF6] text-black w-full h-1/5 pl-5 rounded-xl"
+          />
+          <button
+            type="submit"
+            className="bg-base text-white text-center text-xl font-title w-full h-1/5 pl-5 rounded-xl mt-10"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 export default LoginComponent;
