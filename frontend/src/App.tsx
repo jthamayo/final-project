92b174a1ecdb01a2791547470cca6167c550a893
jthamayo <jtamayobalogh@gmail.com>
2025-05-30 23:15:04 +0200

    refactor: change app routing layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,72 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { User, getCurrentUser } from "./services/UserService.ts";
+import { ACCESS_TOKEN } from "./constants/index.ts";
+
 import ListUserComponent from "./components/user/ListUserComponent";
-import SignInComponent from "./components/auth/SignInComponent";
+import Profile from "./components/user/Profile";
+import SignupComponent from "./components/auth/SignupComponent";
 import LoginComponent from "./components/auth/LoginComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loading from "./common/Loading";
+import NotFound from "./common/NotFound";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    loadCurrentUser();
+  }, []);
+
+  const loadCurrentUser = () => {
+    getCurrentUser()
+      .then((user) => {
+        setCurrentUser(user);
+        setIsAuthenticated(true);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  };
+  const handleLogin = () => {
+    loadCurrentUser();
+    navigate("/");
+  };
+  const handleLogout = (redirectTo = "/") => {
+    localStorage.removeItem(ACCESS_TOKEN);
+    setCurrentUser(null);
+    setIsAuthenticated(false);
+    navigate(redirectTo);
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <BrowserRouter>
-      <main className="">
-        <h1 className="text-blue-300"></h1>
-        <Routes>
-          <Route path="/" element={<LoginComponent />}></Route>
-          <Route path="/users" element={<ListUserComponent />}></Route>
-          <Route path="/signIn" element={<SignInComponent />}></Route>
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <main className="content">
+      <h1 className="text-blue-300"></h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LoginComponent onLogin={handleLogin} onLogout={handleLogout} />
+          }
+        ></Route>
+        <Route path="/users" element={<ListUserComponent />}></Route>
+        <Route path="/signup" element={<SignupComponent />}></Route>
+        <Route
+          path="/users/:username"
+          element={<Profile currentUser={currentUser} />}
+        ></Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   );
 }
 
--- frontend/src/components/auth/SignInComponent.tsx
+++
@@ -1,5 +0,0 @@
-const SigninComponent = () => {
-  return <div className="text-blue-600 text-5xl my-5">Sign In</div>;
-};
-
-export default SigninComponent;
